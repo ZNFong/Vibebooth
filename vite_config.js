17bef383bebdef6eabea8902ca28b95c267680be
ZNFong <ziningfong11@gmail.com>
2026-06-21 17:07:54 +1000

    Update vite_config.js

--- vite_config.js
+++ vite_config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: './',
   build: {
-    outDir: 'dist',
     rollupOptions: {
-      input: './index.html'
+      input: {
+        main: './index.html'
+      }
     }
   }
 })
